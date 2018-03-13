04d6fb87daab6262c5c04f30cc4e0239f7690d40
laudao <nguyen.laura97@gmail.com>
2018-03-13 13:21:52 +0100

    fixed astar and started random strat

--- pySpriteWorld-forStudents/UltimateTicTacToe-new.py
+++ pySpriteWorld-forStudents/UltimateTicTacToe-new.py
@@ -1,13 +1,3 @@
-
-
-
-
-
-
-
-
-
-
 # -*- coding: utf-8 -*-
 
 # Nicolas, 2015-11-18
@@ -35,6 +25,7 @@ from grid2D import *
 
 game = Game()
 
+
 def init(_boardname=None):
     global player,game
     # pathfindingWorld_MultiPlayer4
@@ -55,32 +46,13 @@ def main():
         iterations = int(sys.argv[1])
     print ("Iterations: ")
     print (iterations)
-    vide= [True]*17
-    first=[True,True,False, False,True, False, False, False, True,False,False,False,True,False,False,True,True]
-    sec=[True,True,False,True,True,True, False, True,True,True,False,True,True,True,False,True,True]
-    '''mat=np.array(([[True]*17,
-         [True]*17,
-         [True,True,False, False,True, False, False, False, True,False,False,False,True,False,False,True,True],
-         [True,True,False,True,True,True,True, False, True,True,True,False,True,True,True,False,True,True],
-         [True]*17,
-         [True,True,False,True,True,True,True, False, True,True,True,False,True,True,True,False,True,True],
-         [True,True,False, False, True, False, False, False, True,False,False,False,True,False,False,True,True],
-         [True,True,False,True,True,True,True, False, True,True,True,False,True,True,True,False,True,True],
-         [True]*17,
-         [True,True,False,True,True,True,True, False, True,True,True,False,True,True,True,False,True,True],
-         [True,True,False, False,True, False, False, False, True,False,False,False,True,False,False,True,True],
-         [True,True,False,True,True,True,True, False, True,True,True,False,True,True,True,False,True,True],
-         [True]*17,
-         [True,True,False,True,True,True,True, False, True,True,True,False,True,True,True,False,True,True],
-         [True,True,False, False,True, False, False, False, True,False,False,False,True,False,False,True,True],
-         [True]*17,
-         [True]*17]))'''
+    mur = [False] * 20
+    vide= [False] + [True] * 17 + [False, False]
+    first=[False, True,True,False, False,True, False, False, False, True,False,False,False,True,False,False,True,True, False, False]
+    sec=[False, True,True,False,True,True,True, False, True,True,True,False,True,True,True,False,True,True, False, False]
     
-    mat=np.array([vide,vide,first,sec,vide,sec,first,sec,vide,sec,first,sec,vide,sec,first,vide,vide])    
-   # mat = np.array([[False,False],[True,True]])     
-    #mat = np.ones((17,17))         
-    #np.reshape(mat,(17,17))     
-    #print(mat.shape)
+    mat=np.array((mur,vide,vide,first,sec,vide,sec,first,sec,vide,sec,first,sec,vide,sec,first,vide,vide, mur, mur))
+
     init()
 
         
@@ -98,7 +70,7 @@ def main():
     
     # on localise tous les états initiaux (loc du joueur)
     initStates = [o.get_rowcol() for o in game.layers['joueur']]
-    print ("Init states:", initStates)
+  #  print ("Init states:", initStates)
     
     
     # on localise tous les objets ramassables
@@ -110,7 +82,7 @@ def main():
     # et la zone de jeu pour le tic-tac-toe
     tictactoeStates = [(x,y) for x in range(3,16) for y in range(3,16)]
     #print ("Wall states:", wallStates)
-    print(tictactoeStates)
+ #   print(tictactoeStates)
     # les coordonnees des tiles dans la fiche
     tile_fiole_jaune = (19,1)
     tile_fiole_bleue = (20,1)
@@ -156,9 +128,17 @@ def main():
         return (x,y)
         
         
+    def get_path(noeudFinal, posInit):
+        chemin = []    
     
+        pere = noeudFinal
+        while pere.etat != posInit:
+            chemin.append(pere.etat)
+            pere = pere.pere
     
-    
+        return reversed(chemin)
+
+
     #-------------------------------
     # Boucle principale de déplacements, un joueur apres l'autre
     #-------------------------------
@@ -170,26 +150,31 @@ def main():
     # on place la premiere fiole jaune      
 
     fiole_a_ramasser = put_next_fiole(0,tour)    
-    print(posPlayers[0])
-    print(fiole_a_ramasser)
-    #print(mat)
-    p2 = ProblemeGrid2D(posPlayers[0],fiole_a_ramasser,mat,'manhattan')
-    noeudFinal = probleme.astar(p2)
+
+    morpions = [0] * 9 # les 9 terrains de jeu, 0: matcg nul ou non terminé, 1: joueur 1 a gagné, 2: joueur 2 a gagné
+    terrain = random.randint(0,8) # terrain où va jouer le premier joueur 
     
-    chemin = []    
-    
-    pere = noeudFinal
-    while pere.etat != posPlayers[0]:
-        chemin.append(pere.etat)
-        pere = pere.pere
-    
-    print(chemin)
-    
-    
-    for i in range(iterations):        
+    dict_terrains = {(0,0): (4, 4), (0,1): (4,5) ,(0,2): (4,6), (0,3): (5, 4), (0,4): (5, 5), (0,5): (5, 6), (0,6): (6, 4), (0,7): (6,5),(0:8): (6, 6),
+                     (1,0): (4, 8), (1,1): (4,9) ,(1,2): (4,10), (1,3): (5, 8), (1,4): (5, 9), (1,5): (5, 10), (1,6): (6, 8), (1,7): (6,9),(1:8): (6, 10),
+                     (2,0): (4, 12), (2,1): (4,13) ,(2,2): (4,14), (2,3): (5, 12), (2,4): (5, 13), (2,5): (5, 14), (2,6): (6, 12), (2,7): (6,13),(2:8): (6, 14),
+                     (3,0): (8, 4), (3,1): (8,5) ,(3,2): (8,6), (3,3): (9, 4), (3,4): (9, 5), (3,5): (9, 6), (3,6): (10, 4), (3,7): (10,5),(3:8): (10, 6),
+                     (4,0): (8, 8), (4,1): (8,9) ,(4,2): (8,10), (4,3): (9, 8), (4,4): (9, 9), (4,5): (9, 10), (4,6): (10, 8), (4,7): (10,9),(4:8): (10, 10),    
+                     (5,0): (8, 12), (5,1): (8,13) ,(5,2): (8,14), (5,3): (9, 12), (5,4): (9, 13), (5,5): (9, 14), (5,6): (10, 12), (5,7): (10,13),(5:8): (10, 14),
+                     (5,0): (8, 12), (5,1): (8,13) ,(5,2): (8,14), (5,3): (9, 12), (5,4): (9, 13), (5,5): (9, 14), (5,6): (10, 12), (5,7): (10,13),(5:8): (10, 14),
+   # dictionnaire (terrain, case du terrain) => case dans mat
+   
+   for i in range(iterations):
+        print("Position joueur 0 : ", posPlayers[j])
+        print("Position fiole : ", fiole_a_ramasser)
+       
+        p2 = ProblemeGrid2D(posPlayers[j],fiole_a_ramasser,mat,'manhattan')
+        noeudFinal = probleme.astar(p2)
+
+        chemin = get_path(noeudFinal, posPlayers[j])
+        
         row,col = posPlayers[j]
 
-        for pos in reversed(chemin):
+        for pos in chemin:
             row = pos[0]
             col = pos[1]
             players[j].set_rowcol(row, col)
@@ -199,14 +184,35 @@ def main():
 
             posPlayers[j]=(row,col)
         
-        #o = players[j].ramasse(game.layers) # on la ramasse
+        o = players[j].ramasse(game.layers) # on la ramasse
         game.mainiteration()
-        #print ("Objet de couleur ", couleur(o), " trouvé par le joueur ", j)
+        print ("Objet de couleur ", couleur(o), " trouvé par le joueur ", j)
             
         # ici il faudrait aller la mettre a la position choisie
         # pour jouer a ultimate tic-tac-toe
         # et verifier que la position est legale etc.            
+        
+        random_case = random.randint(0,8) # case du terrain où va jouer le prochain joueur  
             
+        pos_but = dict_cases[(terrain, random_case)]
+        p2 = ProblemeGrid2D(posPlayers[j],pos_but,mat,'manhattan')
+        noeudFinal = probleme.astar(p2)
+        
+        chemin = get_path(noeudFinal, posPlayers[j])
+        
+        row,col = posPlayers[j]
+
+        for pos in chemin:
+            row = pos[0]
+            col = pos[1]
+            players[j].set_rowcol(row, col)
+
+            game.mainiteration()
+
+            posPlayers[j]=(row,col)
+        
+        players[j].depose(game.layers) # on la ramasse
+        game.mainiteration()
             
         # on active le joueur suivant
         # et on place la fiole suivante
@@ -215,7 +221,10 @@ def main():
             tour+=1
         
         fiole_a_ramasser=put_next_fiole(j,tour)    
-    
+        terrain = random_case # prochain terrain
+        
+        if morpions[terrains] != 0:
+            terrain = random.randint(0, 8)
                 
         #break"""
     
